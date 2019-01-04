54ef3c2839ed958e1dc2bb703cbf2976003940c4
Le Alex <alex@beat45.com>
2019-01-04 22:08:29 +0000

    bug fixes

--- s3tools/adjust_video_volumes.rb
+++ s3tools/adjust_video_volumes.rb
@@ -8,7 +8,7 @@ def perform_query(query)
 end
 
 def change_video_volume(in_file, out_file, amount)
-	cmd = "ffmpeg -y -i #{in_file} -vcodec copy -b:a 314k -af \"volume=#{amount}dB\" #{out_file}"
+	cmd = "ffmpeg -y -i #{in_file} -vcodec copy -b:a 314k -af \"volume=#{amount}dB\" \"#{out_file}\""
 	`#{cmd}`
 end
 
@@ -43,43 +43,41 @@ mix_infos = [
 =end
 
 tmp_filename = "/tmp/_mix.mp4"
+tmp_filename2 = "/tmp/__mix.mp4"
 
 # Loop over mix URLs
 mix_infos.each do |mix_info|
 # 	s3 download mix
 	bucket_path = mix_info[:url][/^http.*?\.com\/(.*)$/, 1]
 	puts "Downloading s3://#{bucket_path}..."
-	cmd = "aws s3 cp s3://#{bucket_path} #{tmp_filename}"
+	cmd = "aws s3 cp \"s3://#{bucket_path}\" #{tmp_filename2}"
 	result = `#{cmd}`
 	puts "Done!"
 
 #	update volume mix
-	out_filename = "/tmp/#{File.basename(bucket_path, ".mp4")}_x.mp4"
-	puts "Updating volume and saving to #{out_filename}..."
-	change_video_volume(tmp_filename,
-		out_filename, 
+	puts "Updating volume..."
+	change_video_volume(tmp_filename2,
+		tmp_filename, 
 		mix_info[:volume_offset])
 	puts "Done!"
 
 #	s3 upload mix with new name
+	out_filename = "#{File.basename(bucket_path, ".mp4")}_x.mp4"
+
 	puts "Uploading volume adjusted mix to S3..."
-	cmd = "aws s3 cp #{out_filename} s3://beat45-test-bucket/mixes/#{out_filename} --acl public-read"
+	cmd = "aws s3 cp #{tmp_filename} \"s3://beat45-test-bucket/mixes/#{out_filename}\" --acl public-read"
 	result = `#{cmd}`
 	puts "Done!"
 
-#	Set file permissions to public
-	# cmd = ""
-	# result = `#{cmd}`
-
 #	Calculate new average volume
 	puts "Calculating new average volume..."
-	cmd = "./calculate_volume.rb #{out_filename}"
+	cmd = "./calculate_volume.rb #{tmp_filename}"
 	result = `#{cmd}`
 	new_volume = result[/^.*?: (-?\d+\.\d+)/, 1]
 	puts "Done!"
 
-#	DB update mix_path & avg_volume
+#	DB update mix_path & avg_volume & status
 	puts "Updating DB values..."
-	perform_query "UPDATE video_mix SET avg_volume='#{new_volume}', mix_path='https://s3-us-west-1.amazonaws.com/beat45-test-bucket/mixes/#{out_filename}' WHERE video_id=#{mix_info[:video_id]}"
+	perform_query "UPDATE video_mix SET status='P', avg_volume='#{new_volume}', mix_path='https://s3-us-west-1.amazonaws.com/beat45-test-bucket/mixes/#{out_filename}' WHERE video_id=#{mix_info[:video_id]}"
 	puts "Done!"
 end
